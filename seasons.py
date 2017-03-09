be82a4739c992cdf85d0eb7279168a6ce8d2dc2a
Daniel P. Russo <russodanielp@gmail.com>
2017-03-09 00:01:53 -0500

    added the season reader

--- seasons.py
+++ seasons.py
@@ -4,7 +4,10 @@ import os
 import pandas as pd
 
 # game log dir
-GL_DIR = os.getenv('MLB_DATA') + '\\game_logs'
+
+GL_DIR = os.getenv('MLB_DATA')
+if not GL_DIR:
+    raise Exception("There is no MLB_DATA environmental variable set.")
 
 class Game:
 
@@ -20,11 +23,32 @@ class Season:
     def __init__(self, year, games):
         self.year = year
         self.games = games
+        # dictionary where teams are keys, values are [wins]
+        self.standings = {}
+
+
+    def play_season(self):
+        for game in self.games:
+            home_record = self.standings.get(game.home_team, [0, 0])
+            away_record = self.standings.get(game.away_team, [0, 0])
+            if game.away_score > game.home_score:
+                away_record[0] += 1
+                home_record[1] += 1
+                self.standings[game.away_team] = away_record
+                self.standings[game.home_team] = home_record
+            else:
+                home_record[0] += 1
+                away_record[1] += 1
+                self.standings[game.home_team] = home_record
+                self.standings[game.away_team] = away_record
+
+        for key, value in self.standings.items():
+            print(key, value)
 
 
 def load_season(year):
     """ Return a Season object, by parsing through game log file """
-    path = GL_DIR + "\\GL{0}.txt".format(year)
+    path = GL_DIR + "\\game_logs\\GL{0}.txt".format(year)
     season_info = pd.read_csv(path, index_col=0, header=None)
     games = []
     for game, info in season_info.iterrows():
